b1a58ceb53a1d7de436925da1bb7f50347626d6f
Jack <jackpar1113@gmail.com>
2021-09-18 10:48:07 -0600

    create Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <p>This is a Header</p>
+    </div>
+  );
+}
+
+export default Header;
