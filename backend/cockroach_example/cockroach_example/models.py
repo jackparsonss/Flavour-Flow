455a4c640b7ca32958756589c012ba1fcad154ae
moenuma <moenumasawa@gmail.com>
2021-09-18 16:15:22 -0600

    add User, Ingredient, Recipe class

--- backend/cockroach_example/cockroach_example/models.py
+++ backend/cockroach_example/cockroach_example/models.py
@@ -1,30 +1,76 @@
 from django.db import models
 import uuid
 
-
-class Customers(models.Model):
+class User(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False)
-    name = models.CharField(max_length=250)
+    user_id = models.CharField(max_length=30)
+    password = models.CharField(max_length=32)
+    created_at = models.DateField()
 
 
-class Products(models.Model):
+class Ingredient(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
-        editable=False)
-    name = models.CharField(max_length=250)
-    price = models.DecimalField(max_digits=18, decimal_places=2)
+        editable=False
+    )
+    name = models.CharField(max_lendth=50)
+    aisle = models.CharField(max_length=30)
+    image = models.ImageField()
+
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    unit = models.CharField(max_field=20)
+    name_amount_unit = models.CharField(max_field=80)
 
 
-class Orders(models.Model):
+class Recipe(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
-        editable=False)
-    subtotal = models.DecimalField(max_digits=18, decimal_places=2)
-    customer = models.ForeignKey(
-        Customers, on_delete=models.CASCADE, null=True)
-    product = models.ManyToManyField(Products)
+        editable=False
+    )
+    title = models.CharField(max_length=100)
+    image = models.ImageField()
+    user_id = models.ManyToManyField(User, blank=True)
+
+    ingredients = models.ManyToManyField(Ingredient)
+    ingredient_cnt = models.IntegerField()
+
+    missed_ingredients = models.ManyToManyField(Ingredient)
+    missed_ingredient_cnt = models.IntegerField()
+
+    used_ingredients = models.ManyToManyField(Ingredient)
+    used_ingredient_cnt = models.IntegerField()
+
+    saved_at = models.DateField(blank=True)
+
+
+# class Customers(models.Model):
+#     id = models.UUIDField(
+#         primary_key=True,
+#         default=uuid.uuid4,
+#         editable=False)
+#     name = models.CharField(max_length=250)
+
+
+# class Products(models.Model):
+#     id = models.UUIDField(
+#         primary_key=True,
+#         default=uuid.uuid4,
+#         editable=False)
+#     name = models.CharField(max_length=250)
+#     price = models.DecimalField(max_digits=18, decimal_places=2)
+
+
+# class Orders(models.Model):
+#     id = models.UUIDField(
+#         primary_key=True,
+#         default=uuid.uuid4,
+#         editable=False)
+#     subtotal = models.DecimalField(max_digits=18, decimal_places=2)
+#     customer = models.ForeignKey(
+#         Customers, on_delete=models.CASCADE, null=True)
+#     product = models.ManyToManyField(Products)
