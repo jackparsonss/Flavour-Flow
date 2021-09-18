f20e5868e1adb9401fa4b546d1982d1501ba7424
Jack <jackpar1113@gmail.com>
2021-09-18 15:44:06 -0600

    fix header sizing and add text for bottom of landing page

--- frontend/src/Components/Landing.js
+++ frontend/src/Components/Landing.js
@@ -11,6 +11,15 @@ function Landing() {
         <h1>Get Recipies.</h1>
       </div>
       <SearchRow />
+      <div className="landing__bottom">
+        <div>
+          <h3>Current Ingredients:</h3>
+        </div>
+        <div>
+          <h3>How many recipes?</h3>
+          {/* <Stepper /> */}
+        </div>
+      </div>
     </div>
   );
 }
