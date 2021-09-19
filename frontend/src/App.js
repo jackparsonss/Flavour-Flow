79ca85cce973e66fd5801644c322e8f8d5263230
Jack <jackpar1113@gmail.com>
2021-09-19 00:55:42 -0600

    add authentication page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./Components/Landing";
+import Authentication from "./Components/Authentication/Authentication";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
+          <Route path="/signin">
+            <Authentication login={true} />
+          </Route>
+          <Route path="/signup">
+            <Authentication login={false} />
+          </Route>
           <Route path="/">
             <Landing />
           </Route>
