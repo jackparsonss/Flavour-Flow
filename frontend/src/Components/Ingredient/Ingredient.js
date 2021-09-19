239e35f581fafe996ca798619f8c303dcde8e48a
Jack <jackpar1113@gmail.com>
2021-09-19 09:03:50 -0600

    animate ingredients as they get added

--- frontend/src/Components/Ingredient/Ingredient.js
+++ frontend/src/Components/Ingredient/Ingredient.js
@@ -1,8 +1,14 @@
 import "./Ingredient.css";
+import { animated, useSpring } from "react-spring";
 
 function Ingredient(props) {
+  const styles = useSpring({
+    from: { scaleX: 0, scaleY: 0, opacity: 0 },
+    to: { scaleX: 1, scaleY: 1, opacity: 1 },
+  });
+
   return (
-    <div className="ingredient">
+    <animated.div style={styles} className="ingredient">
       <div
         className="ingredient__x"
         onClick={() => props.removeCallback(props.name)}
@@ -10,7 +16,7 @@ function Ingredient(props) {
         X
       </div>
       <p>{props.name}</p>
-    </div>
+    </animated.div>
   );
 }
 
